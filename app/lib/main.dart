0f68d72a5db868075ed4a27e705d93371e8d52b3
Rohit <rohit1997gupta@gmail.com>
2022-02-20 00:40:53 -0500

    Use new auth view

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dropp/views/authentication/authentication_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,40 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Dropp',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
-      home: const MyHomePage(title: 'Dropp'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Dropp',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
+      home: const AuthenticationView(),
     );
   }
 }
