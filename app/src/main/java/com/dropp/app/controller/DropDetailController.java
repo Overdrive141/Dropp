769cbce950533e368093c5f068354981aee3c235
Rakesh Kumar <kumar4a@uwindsor.ca>
2022-03-04 00:58:51 -0500

    CP8117P53-30: Integrate Firebase AuthToken for Authentication

--- app/src/main/java/com/dropp/app/controller/DropDetailController.java
+++ app/src/main/java/com/dropp/app/controller/DropDetailController.java
@@ -1,8 +1,13 @@
 package com.dropp.app.controller;
 
+import com.dropp.app.common.Constant;
+import com.dropp.app.exception.UserAuthorizationException;
 import com.dropp.app.model.DropDetail;
 import com.dropp.app.model.DropRequest;
 import com.dropp.app.service.DropDetailService;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,12 +29,32 @@ public class DropDetailController {
     }
 
     @PostMapping("/user/{userId}/drop")
-    public DropDetail addDropForUser(@PathVariable @NotNull @NotEmpty Long userId, @RequestBody @Valid DropRequest dropRequest) {
+    public DropDetail addDropForUser(@PathVariable @NotNull @NotEmpty Long userId, @RequestBody @Valid DropRequest dropRequest, @NotNull @NotEmpty @RequestHeader("Authorization") String authorizationHeader) throws FirebaseAuthException {
+        if (!authorizationHeader.startsWith(Constant.BEARER + " ")) {
+            throw new UserAuthorizationException("User not authorized!!");
+        }
+        String[] headerValues = authorizationHeader.split(" ");
+        if (headerValues.length != 2) {
+            throw new UserAuthorizationException("User not authorized!!");
+        }
+        String idToken = headerValues[1];
+        FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
+        String uid = decodedToken.getUid();
         return service.addDrop(userId, dropRequest);
     }
 
     @GetMapping("/user/{userId}/drop")
-    public List<DropDetail> getDropsForUser(@PathVariable @NotNull @NotEmpty Long userId) {
+    public List<DropDetail> getDropsForUser(@PathVariable @NotNull @NotEmpty Long userId, @NotNull @NotEmpty @RequestHeader("Authorization") String authorizationHeader) throws FirebaseAuthException {
+        if (!authorizationHeader.startsWith(Constant.BEARER + " ")) {
+            throw new UserAuthorizationException("User not authorized!!");
+        }
+        String[] headerValues = authorizationHeader.split(" ");
+        if (headerValues.length != 2) {
+            throw new UserAuthorizationException("User not authorized!!");
+        }
+        String idToken = headerValues[1];
+        FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
+        String uid = decodedToken.getUid();
         return service.getDropsForUser(userId);
     }
 }
--- app/src/main/java/com/dropp/app/controller/advice/GenericControllerAdvice.java
+++ app/src/main/java/com/dropp/app/controller/advice/GenericControllerAdvice.java
@@ -1,8 +1,10 @@
 package com.dropp.app.controller.advice;
 
 import com.dropp.app.exception.ResourceNotFoundException;
+import com.dropp.app.exception.UserAuthorizationException;
 import com.dropp.app.exception.UserNotFoundException;
 import com.dropp.app.model.Error;
+import com.google.firebase.auth.FirebaseAuthException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,10 +28,34 @@ public class GenericControllerAdvice {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ResponseBody
-    @ExceptionHandler(UserNotFoundException.class)
+    @ExceptionHandler(ResourceNotFoundException.class)
     public Error handleResourceNotFoundException(ResourceNotFoundException ex) {
         Error error = Error.builder()
                 .errorCode(HttpStatus.NOT_FOUND.value())
+                .message(ex.getMessage())
+                .description(null)
+                .build();
+        return error;
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ResponseBody
+    @ExceptionHandler(UserAuthorizationException.class)
+    public Error handleUserAuthorizationException(UserAuthorizationException ex) {
+        Error error = Error.builder()
+                .errorCode(HttpStatus.UNAUTHORIZED.value())
+                .message(ex.getMessage())
+                .description(null)
+                .build();
+        return error;
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ResponseBody
+    @ExceptionHandler(FirebaseAuthException.class)
+    public Error handleFirebaseAuthException(FirebaseAuthException ex) {
+        Error error = Error.builder()
+                .errorCode(HttpStatus.UNAUTHORIZED.value())
                 .message(ex.getMessage())
                 .description(null)
                 .build();
