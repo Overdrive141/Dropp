723e843165b24ce6be082df680919171a7278890
Rakesh Kumar <kumar4a@uwindsor.ca>
2022-03-18 02:39:38 -0400

    CP8117P53-34: Refactor the metadata getting accepted by REST API

--- app-backend/src/main/java/com/dropp/app/model/UserRequest.java
+++ app-backend/src/main/java/com/dropp/app/model/UserRequest.java
@@ -1,9 +1,12 @@
 package com.dropp.app.model;
 
+import com.dropp.app.model.enums.Avatar;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.validation.annotation.Validated;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -23,19 +26,15 @@ public class UserRequest {
 
     @NotNull
     @NotEmpty
-    @Size(min = 1, max = 35)
-    private String firstName;
-
-    @NotNull
-    @NotEmpty
-    @Size(min = 1, max = 35)
-    private String lastName;
-
-    @NotNull
-    @NotEmpty
     @Email
     private String email;
 
     @Size(min = 10, max = 10)
     private String contactNo;
+
+    @NotEmpty
+    @NotNull
+    @Size(max = 5)
+    @Enumerated(EnumType.ORDINAL)
+    private Avatar avatar;
 }
---
+++ app-backend/src/main/java/com/dropp/app/model/enums/Avatar.java
@@ -0,0 +1,5 @@
+package com.dropp.app.model.enums;
+
+public enum Avatar {
+    AV1, AV2
+}
