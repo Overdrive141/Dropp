c691cdafee140c1f2eaa79bd7c2ddb5eff7cbafe
mehuljethva29@gmail.com <mehuljethva29@gmail.com>
2022-03-22 13:26:41 -0400

    Added Firebase Authentication Credential for access.

--- app-backend/src/main/java/com/dropp/app/Application.java
+++ app-backend/src/main/java/com/dropp/app/Application.java
@@ -7,6 +7,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+
+		try{
+           FileInputStream inputStream = new FileInputStream("src/main/resources/dropp_firebase_key.json");
+           FirebaseOptions options = new FirebaseOptions.Builder()
+                   .setCredentials(GoogleCredentials.fromStream(inputStream))
+                   .build();
+
+           FirebaseApp.initializeApp(options);
+        } catch (Exception error) {
+           error.printStackTrace();
+       }
 		SpringApplication.run(Application.class, args);
 	}
 	
