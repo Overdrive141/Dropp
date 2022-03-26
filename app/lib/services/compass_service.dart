c9fb6bd4d544cbb8015eb894353f8595a580915d
Overdrive141 <linwood2hancock@gmail.com>
2022-03-26 17:44:30 -0400

    BG Location & Compass async fix

--- app/lib/services/compass_service.dart
+++ app/lib/services/compass_service.dart
@@ -1,22 +1,34 @@
+import 'dart:async';
+ 
 import 'package:flutter_compass/flutter_compass.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
+ 
 class CompassService extends StateNotifier<double?> {
   CompassService() : super(null);
-
-  static final provider = StateNotifierProvider<CompassService, double?>((ref) {
+ 
+  StreamSubscription? _streamSubscription;
+ 
+  static final provider =
+      StateNotifierProvider.autoDispose<CompassService, double?>((ref) {
     final compassService = CompassService();
     compassService.trackHeading();
     return compassService;
   });
-
+ 
   void trackHeading() {
-    FlutterCompass.events?.listen((event) {
+    _streamSubscription = FlutterCompass.events?.listen((event) {
       final heading = event.heading ?? 0;
       final bearing = heading < 0 ? 360 + heading : heading;
       if (((state ?? 0) - bearing).abs() > 1) {
         state = bearing;
       }
     });
+  }
+ 
+  @override
+  void dispose() {
+    _streamSubscription?.cancel();
+    _streamSubscription = null;
+    super.dispose();
   }
 }
