1c66674eeb51d8a782fb033e0659e2ea827f9c46
Rohit <rohit1997gupta@gmail.com>
2022-03-26 17:16:53 -0400

    Update assets

--- app/lib/assets.dart
+++ app/lib/assets.dart
@@ -8,4 +8,9 @@ abstract class AppAssets {
   static get avatarFemale => 'assets/images/avatar2.png';
 
   static get marker0 => 'assets/markers/0.png';
+  static get marker1 => 'assets/markers/1.png';
+  static get dropMarker => 'assets/markers/flag.png';
+
+  static get progressAnimation => 'assets/lottie/progress.json';
+  static get bubblesAnimation => 'assets/lottie/bubbles.json';
 }
